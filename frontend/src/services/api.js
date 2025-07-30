80052cda71c44b4e8628f5bd6e2f9ef173472383
girpyx <giwayasir@gmail.com>
2025-07-30 22:26:14 +0100

    fixed issues with api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://127.0.0.1:8000/accounts';
+const BASE_URL = 'http://127.0.0.1:8000/api/accounts';
 
 export const loginUser = (data) => axios.post(`${BASE_URL}/login/`, data);
 export const registerUser = (data) => axios.post(`${BASE_URL}/register/`, data);
